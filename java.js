dceac25d2bdc131027ac64745fa62dbbb65a5914
J-Puente <85426932+J-Puente@users.noreply.github.com>
2022-09-03 00:09:19 -0500

    Variables
    Variables Java

---
+++ java.js
@@ -0,0 +1,21 @@
+var A = "My name is Jay" ;
+B =  
+
+/*
+This is how you block notes 
+var math with lowercase a would be a completely different variable 
+*/ 
+ 
+var B 20;
+
+
+
+/*false is like turning the switch off- No power.
+true==power 
+== means equal */
+
+
+var box = null;
+//the value of nothing//
+
+console.log (sum); 
